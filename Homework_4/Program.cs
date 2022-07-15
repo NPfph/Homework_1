49d275d73244c1c0d965fc684e221165413f1e92
Natasha <a1@MacBook-Pro-Natasha.local>
2022-07-15 15:30:43 +0300

    Fourth task was added

---
+++ Homework_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N
+Console.WriteLine("Введите число n и мы тебе покажем все четные числа от 1 до n");
+int n;
+Console.WriteLine("Введите число n");
+n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < n; i++)
+if (i % 2 == 0)
+{
+    Console.WriteLine(i);
+}
